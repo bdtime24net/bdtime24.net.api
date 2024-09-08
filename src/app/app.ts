098bee0b35e0affa7f4b33b5931e1d1734ab174c
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-09-08 23:21:08 +0600

    CORS is properly configured

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,24 +2,26 @@ import express, { Application } from "express";
 import middleware from "./middleware";
 import routes from "./routes";
 import YAML from "yamljs";
-import cookieParser from "cookie-parser";
 import swaggerUI from "swagger-ui-express";
 import { errorHandler } from "./errors";
 import { notFoundHandler } from "../errors/notFoundError";
+
 const app: Application = express();
 const doc = YAML.load(`${process.cwd()}/src/docs/swagger.yaml`);
 
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(doc));
 
-app.use(express.json());
+middleware.forEach((mw) => app.use(mw));
 
-app.use(middleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 app.use("/api", routes);
+
+
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.use(cookieParser());
 
 export default app;
