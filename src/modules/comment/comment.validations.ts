6bdd307d1c83fad71e7031d2d9dddb66ce015332
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-02 20:57:14 +0600

    pk: as a tcomment init

--- src/modules/comment/comment.validations.ts
+++ src/modules/comment/comment.validations.ts
@@ -2,5 +2,33 @@ import {z} from "zod";
 
 
 export const commentValidationSchema = z.object({
-        
+        id: z.string(),
+        body: z.string(),
+        articleId: z.string(),
+        userId: z.string(),
+        createdAt: z.date(),
+        updatedAt: z.date(),
+        user: z.object({
+            id: z.string(),
+            username: z.string(),
+            avatar: z.string(),
+        }),
+        article: z.object({
+            id: z.string(),
+            headline: z.string(),
+            description: z.string(),
+            sourceName: z.string(),
+            url: z.string(),
+            urlToImage: z.array(z.string()),
+            categoryId: z.string(),
+            userId: z.string(),
+            tagId: z.string(),
+            keywords: z.array(z.string()),
+            createdAt: z.date(),
+            updatedAt: z.date(),
+        }),
     });
+
+
+
+export type TComment = z.infer<typeof commentValidationSchema>;
