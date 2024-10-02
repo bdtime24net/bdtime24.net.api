26b6b917ed1229cca54637f175438bd254cdbf29
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-02 21:04:46 +0600

    commentRoutes aded

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,3 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import {commentValidationSchema} from './comment.validations'
-import {} from './comment.services'
+import { createCommentService } from './comment.services'
+
+
+export const createCommentController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const comment = await createCommentService(req.body);
+        res.status(201).json(comment);
+    } catch (error) {
+        next(error);
+    }
+}
--- src/modules/comment/comment.routes.ts
+++ src/modules/comment/comment.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 const commenRoutes  : Router = Router();
+import {createCommentController} from './comment.controller'
 
+commenRoutes.post("/comment/create", createCommentController)
 
 
 export default commenRoutes;
