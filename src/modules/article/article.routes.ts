746aed38efaadd30431d369435ae2a9b06fbd503
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-28 11:10:29 +0600

    yarn build-1

--- src/modules/article/article.routes.ts
+++ src/modules/article/article.routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import { createArticleController, getArticlesController, getArticleByIdController, updateArticleController, deleteArticleController } from "./article.controller";
-import veryfyToken from '../../middlewares/auth.middleware'
+// import veryfyToken from '../../middlewares/auth.middleware'
 
 
 
 const articleRoutes: Router = Router();
 
-articleRoutes.post("/article/create", veryfyToken, createArticleController);
+articleRoutes.post("/article/create", createArticleController);
 
 articleRoutes.get("/article", getArticlesController);
 
 
 articleRoutes.get("/article/:id", getArticleByIdController);
 
-articleRoutes.put("/article/:id", veryfyToken, updateArticleController);
+articleRoutes.put("/article/:id", updateArticleController);
 
-articleRoutes.delete("/article/:id", veryfyToken, deleteArticleController);
+articleRoutes.delete("/article/:id", deleteArticleController);
 
 
 
